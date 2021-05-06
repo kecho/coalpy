60a846b0ba2c657cbd394de3725b15125e4a10b8
Kleber Garcia <kleber.garcia@unity3d.com>
2021-05-05 23:35:02 -0400

    Test case filtering

--- Source/tests/main.cpp
+++ Source/tests/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <coalpy.core/Assert.h>
+#include <coalpy.core/ClParser.h>
+#include <coalpy.core/ClTokenizer.h>
 #include <string>
 #include <set>
 #include <atomic>
@@ -12,12 +14,19 @@
 
 using namespace coalpy;
 
+typedef TestSuite*(*CreateSuiteFn)();
+
 namespace coalpy
 {
 
 TestSuite* fileSystemSuite();
 
 }
+
+CreateSuiteFn g_suites[] = {
+    fileSystemSuite
+};
+
 
 std::atomic<int> g_errors = 0;
 void assertHandler(const char* condition, const char* fileStr, int line, const char* msg)
@@ -26,15 +35,11 @@ void assertHandler(const char* condition, const char* fileStr, int line, const c
     ++g_errors;
 }
 
-
-typedef TestSuite*(*CreateSuiteFn)();
-
 struct Filters
 {
     std::set<std::string> suites;
     std::set<std::string> cases;
 } gFilters;
-
 
 void runSuite(CreateSuiteFn createFn)
 {
@@ -65,10 +70,82 @@ void runSuite(CreateSuiteFn createFn)
     delete suite;
 }
 
+struct ArgParameters
+{
+    bool help = false;
+    bool printTests = false;
+    const char* suitefilter = "";
+    const char* testfilter = "";
+};
+
+bool prepareCli(ClParser& p, ArgParameters& params)
+{
+    ClParser::GroupId gid= p.createGroup("General", "General Params:");
+    p.bind(gid, &params);
+    CliSwitch(gid, "help", "h", "help", Bool, ArgParameters, help);
+    CliSwitch(gid, "print available suites and tests", "p", "printtests", Bool, ArgParameters, printTests);
+    CliSwitch(gid, "Comma separated suite filters", "s", "suites", String, ArgParameters, suitefilter);
+    CliSwitch(gid, "Comma separated test case filters", "t", "tests", String, ArgParameters, testfilter);
+    return true;
+}
 
 int main(int argc, char* argv[])
 {
+    ArgParameters params;
+    ClParser p;
+    if (!prepareCli(p, params))
+    {
+        std::cerr << "Error setting up cli parser\n";
+        return -1;
+    }
+
+    if (!p.parse(argc, argv))
+    {
+        return -1;
+    }
+
+    if (params.help)
+    {
+        p.prettyPrintHelp();
+        return 0;
+    }
+
+    int suiteCounts = (int)(sizeof(g_suites)/sizeof(g_suites[0]));
+    if (params.printTests)
+    {
+        
+        for (int i = 0; i < suiteCounts; ++i)
+        {
+            TestSuite* suite = g_suites[i]();
+            printf("%s:\n", suite->name());
+            int caseCount = 0;
+            const TestCase* cases = suite->getCases(caseCount);
+            for (int j = 0; j < caseCount; ++j)
+                printf("   %s\n", cases[j].name);
+            printf("\n", suite->name());
+            delete suite;
+        }
+        return 0;
+    }
+
+    {
+        std::string suiteFilterStr = params.suitefilter;
+        auto strList = ClTokenizer::splitString(suiteFilterStr, ',');
+        for (auto s : strList)
+            gFilters.suites.insert(s);
+    }
+
+    {
+        std::string caseFilterStr = params.testfilter;
+        auto strList = ClTokenizer::splitString(caseFilterStr, ',');
+        for (auto s : strList)
+            gFilters.cases.insert(s);
+    }
+
     AssertSystem::setAssertHandler(assertHandler);
-    runSuite(fileSystemSuite);
+    for (int i = 0; i < suiteCounts; ++i)
+    {
+        runSuite(g_suites[i]);
+    }
     return 0;
 }
