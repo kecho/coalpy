6fd66e98bf9e6f72d1cc5ca2cec784b89f412e68
Kleber Garcia <kleber.garcia@unity3d.com>
2021-06-20 23:25:23 -0400

    Tabs & spaces

--- Source/modules/render/dx12/Dx12GpuMemPools.h
+++ Source/modules/render/dx12/Dx12GpuMemPools.h
@@ -12,87 +12,87 @@ namespace render
 struct Dx12GpuMemoryBlock
 {
     size_t uploadSize = 0ull;
-	void* mappedBuffer = nullptr;
-	uint64_t offset = 0ull;
-	ID3D12Resource* buffer = nullptr;
-	D3D12_GPU_VIRTUAL_ADDRESS gpuVA = 0;
+    void* mappedBuffer = nullptr;
+    uint64_t offset = 0ull;
+    ID3D12Resource* buffer = nullptr;
+    D3D12_GPU_VIRTUAL_ADDRESS gpuVA = 0;
 };
 
 class Dx12GpuUploadPool
 {
 public:
-	Dx12GpuUploadPool(Dx12Device& device, ID3D12CommandQueue& queue, uint64_t initialPoolSize);
-	~Dx12GpuUploadPool();
-	void beginUsage();
-	void endUsage();
+    Dx12GpuUploadPool(Dx12Device& device, ID3D12CommandQueue& queue, uint64_t initialPoolSize);
+    ~Dx12GpuUploadPool();
+    void beginUsage();
+    void endUsage();
 
-	Dx12GpuMemoryBlock allocUploadBlock(size_t sizeBytes);
+    Dx12GpuMemoryBlock allocUploadBlock(size_t sizeBytes);
 
 private:
-	class Dx12GpuUploadPoolImpl* m_impl;
+    class Dx12GpuUploadPoolImpl* m_impl;
 };
 
 struct Dx12GpuDescriptorTable
 {
-	D3D12_CPU_DESCRIPTOR_HANDLE cpuHandle = {};
-	D3D12_GPU_DESCRIPTOR_HANDLE gpuHandle = {};
-	ID3D12DescriptorHeap* ownerHeap = nullptr;
-	uint32_t descriptorCounts = 0u;
-	uint32_t descriptorSize = 0u;
+    D3D12_CPU_DESCRIPTOR_HANDLE cpuHandle = {};
+    D3D12_GPU_DESCRIPTOR_HANDLE gpuHandle = {};
+    ID3D12DescriptorHeap* ownerHeap = nullptr;
+    uint32_t descriptorCounts = 0u;
+    uint32_t descriptorSize = 0u;
 
-	bool valid() const
-	{
-		return cpuHandle.ptr != 0;
-	}
+    bool valid() const
+    {
+        return cpuHandle.ptr != 0;
+    }
 
-	bool isShaderVisible() const
-	{
-		return gpuHandle.ptr != 0;
-	}
+    bool isShaderVisible() const
+    {
+        return gpuHandle.ptr != 0;
+    }
 
-	D3D12_CPU_DESCRIPTOR_HANDLE getCpuHandle(uint32_t index = 0) const
-	{
-		CPY_ASSERT(index < descriptorCounts);
-		D3D12_CPU_DESCRIPTOR_HANDLE result = { cpuHandle.ptr + index * descriptorSize };
-		return result;
-	}
+    D3D12_CPU_DESCRIPTOR_HANDLE getCpuHandle(uint32_t index = 0) const
+    {
+        CPY_ASSERT(index < descriptorCounts);
+        D3D12_CPU_DESCRIPTOR_HANDLE result = { cpuHandle.ptr + index * descriptorSize };
+        return result;
+    }
 
-	D3D12_GPU_DESCRIPTOR_HANDLE getGpuHandle(uint32_t index = 0) const
-	{
-		CPY_ASSERT(index < descriptorCounts);
-		D3D12_GPU_DESCRIPTOR_HANDLE result = { gpuHandle.ptr + index * descriptorSize };
-		return result;
-	}
+    D3D12_GPU_DESCRIPTOR_HANDLE getGpuHandle(uint32_t index = 0) const
+    {
+        CPY_ASSERT(index < descriptorCounts);
+        D3D12_GPU_DESCRIPTOR_HANDLE result = { gpuHandle.ptr + index * descriptorSize };
+        return result;
+    }
 
-	void advance(uint32_t count)
-	{
-		if (count >= descriptorCounts)
-			return;
-		cpuHandle = getCpuHandle(count);
-		gpuHandle = getGpuHandle(count);
-		descriptorCounts -= count;
-	}
+    void advance(uint32_t count)
+    {
+        if (count >= descriptorCounts)
+            return;
+        cpuHandle = getCpuHandle(count);
+        gpuHandle = getGpuHandle(count);
+        descriptorCounts -= count;
+    }
 };
 
 class Dx12GpuDescriptorTablePool
 {
 public:
-	Dx12GpuDescriptorTablePool(
+    Dx12GpuDescriptorTablePool(
         Dx12Device& device,
         ID3D12CommandQueue& queue,
         uint32_t maxDescriptorCount,
         D3D12_DESCRIPTOR_HEAP_TYPE heapType);
 
-	~Dx12GpuDescriptorTablePool();
+    ~Dx12GpuDescriptorTablePool();
 
-	void beginUsage();
-	void endUsage();
-	Dx12GpuDescriptorTable allocateTable(uint32_t tableSize);
-	const Dx12GpuDescriptorTable& lastAllocatedTable() const { return m_lastTable; }
+    void beginUsage();
+    void endUsage();
+    Dx12GpuDescriptorTable allocateTable(uint32_t tableSize);
+    const Dx12GpuDescriptorTable& lastAllocatedTable() const { return m_lastTable; }
 
 private:
-	class Dx12GpuDescriptorTablePoolImpl* m_impl;
-	Dx12GpuDescriptorTable m_lastTable;
+    class Dx12GpuDescriptorTablePoolImpl* m_impl;
+    Dx12GpuDescriptorTable m_lastTable;
 };
 
 }
