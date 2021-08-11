f2c2006b382e9c38db79ea96911b124412497306
Kleber Garcia <kleber.garcia@unity3d.com>
2021-08-11 10:12:36 -0400

    Seascape example

---
+++ Source/scripts/coalpy/examples/seascape.hlsl
@@ -0,0 +1,235 @@
+/*
+ * "Seascape" by Alexander Alekseev aka TDM - 2014
+ * License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ * Contact: tdmaav@gmail.com
+translated to hlsl by Kleber Garcia
+ */
+
+#define NUM_STEPS 8
+#define PI	 	3.141592
+#define EPSILON	1e-3
+#define EPSILON_NRM (0.1 / iResolution.x)
+#define AA
+
+//HLSL translations
+#define vec2 float2
+#define vec3 float3
+#define vec4 float4
+#define mat2 float2x2
+#define mat3 float3x3
+#define mix lerp
+#define iMouse float2(0, 0)
+
+cbuffer Constants : register(b0)
+{
+    float2 iResolution;
+    float iTime;
+    float unused;
+
+    float g_specular;
+    float g_diffuse;
+    float g_refraction;
+    float g_unused;
+}
+
+// sea
+#define ITER_GEOMETRY  3
+#define ITER_FRAGMENT  5
+#define SEA_HEIGHT  0.6
+#define SEA_CHOPPY  4.0
+#define SEA_SPEED  0.8
+#define SEA_FREQ  0.16
+#define SEA_BASE  vec3(0.0,0.09,0.18)
+#define SEA_WATER_COLOR  vec3(0.8,0.9,0.6)*0.6
+#define SEA_TIME (1.0 + iTime * SEA_SPEED)
+#define octave_m  mat2(1.6,1.2,-1.2,1.6)
+
+// math
+mat3 fromEuler(vec3 ang) {
+	vec2 a1 = vec2(sin(ang.x),cos(ang.x));
+    vec2 a2 = vec2(sin(ang.y),cos(ang.y));
+    vec2 a3 = vec2(sin(ang.z),cos(ang.z));
+    mat3 m;
+    m[0] = vec3(a1.y*a3.y+a1.x*a2.x*a3.x,a1.y*a2.x*a3.x+a3.y*a1.x,-a2.y*a3.x);
+	m[1] = vec3(-a2.y*a1.x,a1.y*a2.y,a2.x);
+	m[2] = vec3(a3.y*a1.x*a2.x+a1.y*a3.x,a1.x*a3.x-a1.y*a3.y*a2.x,a2.y*a3.y);
+	return m;
+}
+float hash( vec2 p ) {
+	float h = dot(p,vec2(127.1,311.7));	
+    return frac(sin(h)*43758.5453123);
+}
+float noise( in vec2 p ) {
+    vec2 i = floor( p );
+    vec2 f = frac( p );	
+	vec2 u = f*f*(3.0-2.0*f);
+    return -1.0+2.0*mix( mix( hash( i + vec2(0.0,0.0) ), 
+                     hash( i + vec2(1.0,0.0) ), u.x),
+                mix( hash( i + vec2(0.0,1.0) ), 
+                     hash( i + vec2(1.0,1.0) ), u.x), u.y);
+}
+
+// lighting
+float diffuse(vec3 n,vec3 l,float p) {
+    return pow(max(dot(n,l) * 0.4 + 0.6,0.0),p);
+}
+float specular(vec3 n,vec3 l,vec3 e,float s) {    
+    float nrm = (s + 8.0) / (PI * 8.0);
+    return pow(max(dot(reflect(e,n),l),0.0),s) * nrm;
+}
+
+// sky
+vec3 getSkyColor(vec3 e) {
+    e.y = (max(e.y,0.0)*0.8+0.2)*0.8;
+    return vec3(pow(1.0-e.y,2.0), 1.0-e.y, 0.6+(1.0-e.y)*0.4) * 1.1;
+}
+
+// sea
+float sea_octave(vec2 uv, float choppy) {
+    uv += noise(uv);        
+    vec2 wv = 1.0-abs(sin(uv));
+    vec2 swv = abs(cos(uv));    
+    wv = mix(wv,swv,wv);
+    return pow(1.0-pow(wv.x * wv.y,0.65),choppy);
+}
+
+float map(vec3 p) {
+    float freq = SEA_FREQ;
+    float amp = SEA_HEIGHT;
+    float choppy = SEA_CHOPPY;
+    vec2 uv = p.xz; uv.x *= 0.75;
+    
+    float d, h = 0.0;    
+    for(int i = 0; i < ITER_GEOMETRY; i++) {        
+    	d = sea_octave((uv+SEA_TIME)*freq,choppy);
+    	d += sea_octave((uv-SEA_TIME)*freq,choppy);
+        h += d * amp;        
+    	uv = mul(uv, octave_m); freq *= 1.9; amp *= 0.22;
+        choppy = mix(choppy,1.0,0.2);
+    }
+    return p.y - h;
+}
+
+float map_detailed(vec3 p) {
+    float freq = SEA_FREQ;
+    float amp = SEA_HEIGHT;
+    float choppy = SEA_CHOPPY;
+    vec2 uv = p.xz; uv.x *= 0.75;
+    
+    float d, h = 0.0;    
+    for(int i = 0; i < ITER_FRAGMENT; i++) {        
+    	d = sea_octave((uv+SEA_TIME)*freq,choppy);
+    	d += sea_octave((uv-SEA_TIME)*freq,choppy);
+        h += d * amp;        
+    	uv = mul(uv, octave_m); freq *= 1.9; amp *= 0.22;
+        choppy = mix(choppy,1.0,0.2);
+    }
+    return p.y - h;
+}
+
+vec3 getSeaColor(vec3 p, vec3 n, vec3 l, vec3 eye, vec3 dist) {  
+    float fresnel = clamp(1.0 - dot(n,-eye), 0.0, 1.0);
+    fresnel = pow(fresnel,3.0) * 0.5;
+        
+    vec3 reflected = getSkyColor(reflect(eye,n));    
+    vec3 refracted = g_refraction * (SEA_BASE + g_diffuse * diffuse(n,l,80.0) * SEA_WATER_COLOR * 0.12); 
+    //
+    vec3 color = mix(refracted,reflected,fresnel);
+    
+    float atten = max(1.0 - dot(dist,dist) * 0.001, 0.0);
+    color += max(SEA_WATER_COLOR * (p.y - SEA_HEIGHT), 0.0) * 0.18 * atten;
+    //
+    color += vec3(g_specular * specular(n,l,eye,60.0).xxx);
+    
+    return color;
+}
+
+// tracing
+vec3 getNormal(vec3 p, float eps) {
+    vec3 n;
+    n.y = map_detailed(p);    
+    n.x = map_detailed(vec3(p.x+eps,p.y,p.z)) - n.y;
+    n.z = map_detailed(vec3(p.x,p.y,p.z+eps)) - n.y;
+    n.y = eps;
+    return normalize(n);
+}
+
+float heightMapTracing(vec3 ori, vec3 dir, out vec3 p) {  
+    float tm = 0.0;
+    float tx = 1000.0;    
+    float hx = map(ori + dir * tx);
+    if(hx > 0.0) return tx;   
+    float hm = map(ori + dir * tm);    
+    float tmid = 0.0;
+    for(int i = 0; i < NUM_STEPS; i++) {
+        tmid = mix(tm,tx, hm/(hm-hx));                   
+        p = ori + dir * tmid;                   
+    	float hmid = map(p);
+		if(hmid < 0.0) {
+        	tx = tmid;
+            hx = hmid;
+        } else {
+            tm = tmid;
+            hm = hmid;
+        }
+    }
+    return tmid;
+}
+
+vec3 getPixel(in vec2 coord, float time) {    
+    vec2 uv = coord / iResolution.xy;
+    uv = uv * 2.0 - 1.0;
+    uv.x *= iResolution.x / iResolution.y;    
+        
+    // ray
+    vec3 ang = vec3(sin(time*3.0)*0.1,sin(time)*0.2+0.3,time);    
+    vec3 ori = vec3(0.0,3.5,time*5.0);
+    vec3 dir = normalize(vec3(uv.xy,-2.0)); dir.z += length(uv) * 0.14;
+    dir = mul(normalize(dir),  fromEuler(ang));
+    
+    // tracing
+    vec3 p;
+    heightMapTracing(ori,dir,p);
+    vec3 dist = p - ori;
+    vec3 n = getNormal(p, dot(dist,dist) * EPSILON_NRM);
+    vec3 light = normalize(vec3(0.0,1.0,0.8)); 
+             
+    // color
+    return mix(
+        getSkyColor(dir),
+        getSeaColor(p,n,light,dir,dist),
+    	pow(smoothstep(0.0,-0.02,dir.y),0.2));
+}
+
+// main
+void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+    float time = iTime * 0.3 + iMouse.x*0.01;
+	
+#ifdef AA
+    vec3 color = vec3(0.0.xxx);
+    for(int i = -1; i <= 1; i++) {
+        for(int j = -1; j <= 1; j++) {
+        	vec2 uv = fragCoord+vec2(i,j)/3.0;
+    		color += getPixel(uv, time);
+        }
+    }
+    color /= 9.0;
+#else
+    vec3 color = getPixel(fragCoord, time);
+#endif
+    
+    // post
+	fragColor = vec4(pow(color,vec3(0.65.xxx)), 1.0);
+}
+
+RWTexture2D<float4> g_outputTarget : register(u0);
+
+[numthreads(8,4,1)]
+void main(int3 dti : SV_DispatchThreadID)
+{
+    vec4 outColor;
+    vec2 coords = vec2(dti.x, iResolution.y - dti.y - 1);
+    mainImage(outColor, coords);
+    g_outputTarget[dti.xy] = outColor;
+}
+
---
+++ Source/scripts/coalpy/examples/seascape.py
@@ -0,0 +1,55 @@
+import coalpy.gpu as gpu
+import math
+
+print ("<<coalpy seascape>>")
+print ("graphics devices:")
+[print("{}: {}".format(idx, nm)) for (idx, nm) in gpu.get_adapters()]
+
+info = gpu.get_current_adapter_info()
+print("Current device: {}".format(info[1]))
+
+seascapeShader = gpu.Shader(file="examples/seascape.hlsl", name="seascape")
+output_table = None
+
+speed = 0.1
+specular = 1.0
+diffuse = 1.0
+refraction = 1.0
+
+def buildUi(imgui):
+    global speed, specular, diffuse, refraction
+
+    imgui.begin("Settings")
+    speed = imgui.slider_float("speed", speed, 0.0, 1.0)
+    specular = imgui.slider_float("specular", specular, 0.0, 1.0)
+    diffuse = imgui.slider_float("diffuse", diffuse, 0.0, 1.0)
+    refraction = imgui.slider_float("refraction", refraction, 0.0, 1.0)
+    imgui.end()
+
+
+def on_render(render_args : gpu.RenderArgs):
+    global output_table
+    global speed, specular, diffuse, refraction
+
+    buildUi(render_args.imgui)
+
+    cmdList = gpu.CommandList()
+    xv = int(math.ceil((render_args.width)/8));
+    yv = int(math.ceil((render_args.height)/4));
+    cmdList.dispatch(
+        x = xv, y =yv, z = 1,
+        constants = [
+            float(render_args.width), float(render_args.height), float(0.04 * speed * render_args.render_time), 0.0,
+            specular, diffuse, refraction, 0.0
+        ],
+        shader = seascapeShader,
+        output_tables = output_table
+    )
+
+    gpu.schedule([cmdList])
+
+window = gpu.Window("seascape example", 1920, 1080, on_render)
+output_table = gpu.OutResourceTable("output table", [window.display_texture])
+
+if __name__ == "__main__":
+    gpu.run()
