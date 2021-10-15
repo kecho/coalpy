419b7d88b695dfa072473a12b1781abe62c184dc
Kleber Garcia <kleber.garcia@unity3d.com>
2021-10-15 13:17:45 -0400

    documentation on resource download request

--- Source/pymodules/gpu/ResourceDownloadRequest.cpp
+++ Source/pymodules/gpu/ResourceDownloadRequest.cpp
@@ -20,10 +20,10 @@ namespace methods
 }
 
 static PyMethodDef g_resourceDownloadMethods[] = {
-    KW_FN(resolve, resolve,R"()"),
-    KW_FN(is_ready, isReady,R"()"),
-    KW_FN(data_as_bytearray, dataAsByteArray,R"()"),
-    KW_FN(data_byte_row_pitch, dataByteRowPitch,R"()"),
+    KW_FN(resolve, resolve,R"(Waits for binary data of the texture to be downloaded. This method will block the CPU until the GPU has finished downloading the data. After this call, check availability with is_ready and then get the data as desired. )"),
+    KW_FN(is_ready, isReady,R"( Polls the GPU (internally a fence) to check if the data is ready. When doing async this method can be used to query until we have data ready.)"),
+    KW_FN(data_as_bytearray, dataAsByteArray,R"( returns the data as a bytearray. This Byte array is internally cached and referenced by the ResourceDownload object.)"),
+    KW_FN(data_byte_row_pitch, dataByteRowPitch,R"( Assuming the data is ready, this method returns the row pitch (in case the resource is a texture).)"),
     FN_END
 };
 
