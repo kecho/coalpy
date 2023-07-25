6200ef2fbbf1f4d645fc4ddaa9d2337be26afcc9
Kleber Garcia <kleber.garcia@unity3d.com>
2023-07-25 15:30:09 -0400

    Documentation tool

--- Source/scripts/doctool/doctool.py
+++ Source/scripts/doctool/doctool.py
@@ -35,12 +35,12 @@ def html_header(module_name):
 
                             if (children.style.display == 'none')
                             {
-                                if (this.dot != null) this.dot.innerHTML = "+";
+                                if (this.dot != null) this.dot.innerHTML = "-";
                                 children.style.display = 'block';
                             }
                             else
                             {
-                                if (this.dot != null) this.dot.innerHTML = "-";
+                                if (this.dot != null) this.dot.innerHTML = "+";
                                 children.style.display = 'none';
                             }
                         });
@@ -50,8 +50,45 @@ def html_header(module_name):
     </head>
 """ % (module_name), end="")
 
-def html_doc_bar_item_name(name, can_expand=True):
-    return """<div class="treelink">%s<span>%s</span></div>""" % ('<div class="treedot">+</div>' if can_expand else "", name)
+def html_doc_bar_item_name(name, can_expand=True, is_expanded=True):
+    return """<div class="treelink"><div class="treedot">%s</div><span>%s</span></div>""" % (('-' if is_expanded else '+') if can_expand else "", name)
+
+def html_class_doc_bar(class_name, class_obj, lvl = 0):
+    allsymbols = dir(class_obj)
+    publicsymbols = [(nm, getattr(class_obj, nm)) for nm in allsymbols if not (nm.startswith("__") or nm.startswith("Enum"))]
+    methodsymbols = [(nm, s) for (nm, s) in publicsymbols if callable(s) and type(s) != type]
+    subtypesymbols = [(nm, s) for (nm, s) in publicsymbols if (type(s) == type and not type(s).__name__.startswith("Enum"))]
+    members = [(nm, s) for (nm, s) in publicsymbols if (not type(s).__name__.startswith("Enum")) and (not type(s) == type) and (not callable(s))]
+    hasChildren = len(members) > 0 or len(subtypesymbols) > 0 or len(methodsymbols) > 0
+    s = html_doc_bar_item_name(class_name, True, not hasChildren)
+    if hasChildren:
+        if (lvl > 0):
+            s += """
+            <ul>"""
+        else:
+            s += """
+            <ul style="display:none">"""
+        for (nm, symb) in members:
+            s += """
+            <li>""" 
+            s += html_class_doc_bar(nm, symb, lvl + 1)
+            s += """
+            </li>""" 
+        for (nm, symb) in methodsymbols:
+            s += """
+            <li>""" 
+            s += html_class_doc_bar(nm, symb, lvl + 1)
+            s += """
+            </li>""" 
+        for (nm, symb) in subtypesymbols:
+            s += """
+            <li>""" 
+            s += html_class_doc_bar(nm, symb, lvl + 1)
+            s += """
+            </li>""" 
+        s += """
+        </ul>"""
+    return s
 
 def html_doc_bar(mod):
     print("""
@@ -87,7 +124,7 @@ def html_doc_bar(mod):
     for (nm, s) in typesymbols:
         print("""
                     <li>""", end="")
-        print(html_doc_bar_item_name(nm, False), end="")
+        print(html_class_doc_bar(nm, s), end="")
         print("""
                     </li>""", end="")
     print("""
@@ -116,12 +153,42 @@ def html_doc_bar(mod):
     print("""
         </div>""", end="")
 
+def html_recurse_doc(parent_name, obj_name, obj):
+    outs = "<h3>%s</h3>" % (parent_name + obj_name)
+    outs += "<pre>%s</pre>" % (obj.__doc__)
+    outs += "<br/>"
+    allsymbols = dir(obj)
+    publicsymbols = [(nm, getattr(obj, nm)) for nm in allsymbols if not (nm.startswith("__") or nm.startswith("Enum"))]
+    funcsymbols = [(nm, s) for (nm, s) in publicsymbols if callable(s) and type(s) != type]
+    typesymbols = [(nm, s) for (nm, s) in publicsymbols if (type(s) == type and not type(s).__name__.startswith("Enum"))]
+    enumsymbols = [(nm, s) for (nm, s) in publicsymbols if (type(s).__name__.startswith("Enum"))]
+    members = [(nm, s) for (nm, s) in publicsymbols if (not type(s).__name__.startswith("Enum")) and (not type(s) == type) and (not callable(s))]
+    new_parent_name = parent_name + obj_name + "."
 
+    for (nm, s) in members:
+        outs += ("<div>%s</div><br/>" % nm)
+
+    for (nm, s) in funcsymbols:
+        outs += html_recurse_doc(new_parent_name, nm, s) 
+    for (nm, s) in typesymbols:
+        outs += html_recurse_doc(new_parent_name, nm, s) 
+    for (nm, s) in enumsymbols:
+        outs += html_recurse_doc(new_parent_name, nm, s) 
+    return outs
+
+def html_doc_content(mod):
+    print("""
+        <div class="content">""")
+    print(html_recurse_doc("", mod.__name__, mod))
+    print("""
+        </div>""")
+    
 
 def html_body(mod):
     print("""
     <body>""", end="")
     html_doc_bar(mod)
+    html_doc_content(mod)
     print("""
     </body>""", end="")
 
